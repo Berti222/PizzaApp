df3fbc1ff4adba370e8e25b8c76380dbd8ae7d41
Agoston Bertol <agostonbertold@gmail.com>
2020-02-21 11:29:59 +0100

    Feat: Deliverers can sent

--- PizzaApp/Controllers/DelivererController.cs
+++ PizzaApp/Controllers/DelivererController.cs
@@ -23,8 +23,49 @@ namespace PizzaApp.Controllers
         // GET: Deliverer
         public ActionResult Index()
         {
+            List<Deliverer> deliverers = _context.Deliverers.ToList();
+            return View(deliverers);
+        }
 
-            return View();
+        public ActionResult Send(int id)
+        {
+            Deliverer delivererInDB = _context.Deliverers.Where(x => x.Id == id).SingleOrDefault();
+
+            List<Order> orders = _context
+                    .Orders
+                    .Where(o => o.Status == StatusName.VaitingForDelivering
+                    || o.Status == StatusName.Active)                    
+                    .ToList();
+
+            if (delivererInDB.IsOnWay == false)
+            {
+                foreach (Order order in orders)
+                {
+                    if(order.Status == StatusName.VaitingForDelivering && order.DelivererId == delivererInDB.Id)
+                    {
+                        order.Status = StatusName.Active;
+                    }
+                }
+                delivererInDB.IsOnWay = true;
+            }
+            else
+            {
+                foreach (Order order in orders)
+                {
+                    if (order.Status == StatusName.Active && order.DelivererId == delivererInDB.Id)
+                    {
+                        order.Status = StatusName.Delivered;
+                    }
+                }
+                if(delivererInDB.IsFull == true)                
+                    delivererInDB.IsFull = false;
+                
+                delivererInDB.IsOnWay = false;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- PizzaApp/Models/Deliverer.cs
+++ PizzaApp/Models/Deliverer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,16 @@ namespace PizzaApp.Models
     public class Deliverer
     {
         public int Id { get; set; }
+        [Display(Name ="Név")]
         public string Name { get; set; }
+        [Display(Name = "Jármű")]
         public string Vehicle { get; set; }
-
+        [Display(Name = "Úton van?")]
         public bool IsOnWay { get; set; }
+        [Display(Name = "Capacítás")]
         public int Capacity { get; set; }
+        [Display(Name = "Tele van?")]
         public bool IsFull { get; set; }
 
-        public List<Order> Orders { get; set; }
     }
 }
